088b41e3adee3140e129d47f7fe0367875ec312e
Mark <m5145692923@gmail.com>
2021-08-09 15:46:56 -0400

    add hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ke Ma
+ */
+public class Hello {
+    public static void main(String[] agrs){
+        System.out.print("Hello,\n");
+        System.out.print("My name is Ke Ma.\n");
+        System.out.print("nice to meet you.\n");
+    }
+    
+}
